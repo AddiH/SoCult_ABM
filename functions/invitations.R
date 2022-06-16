810302eec9bde26a7cbcba1b6e9c674ba7fd28b9
AnnoyingAstrid <72738864+AddiH@users.noreply.github.com>
2022-06-16 15:29:01 +0200

    split succes

---
+++ functions/invitations.R
@@ -0,0 +1,17 @@
+
+
+simple.sort <- function(invit, dyads, sort) {
+for (agent in invit$agent){ # each agent sends invites
+  choice <- dyads %>% # make a df from dyads
+    filter(agent_1 == agent) %>% # only choose current agent
+    filter(agent_2 %in% invit$agent) %>%  # remove agents that already have a date (are no longer in invit)
+    arrange(desc(.data[[sort]])) %>%  # sort so agent with higest sort is on top
+    filter(.data[[sort]] == .data[[sort]][1]) # choose all agents that have the same sort as the top agent
+  
+  n <- sample(1:nrow(choice),1) # 1 random number between 1 and the number of rows in choice
+  invit_agent <- choice$agent_2[n] # select random agent from choice
+  agent_index <- which(invit$agent == agent) # get the row number for the current agent in invit
+  invit$to[agent_index] <- invit_agent # in invit set agent choice 
+}
+  return(invit)
+}
