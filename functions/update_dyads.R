a2845f1d027f8376464d87689d5fc9dc860a55b7
AnnoyingAstrid <72738864+AddiH@users.noreply.github.com>
2022-06-21 15:28:14 +0200

    ...

--- functions/update_dyads.R
+++ functions/update_dyads.R
@@ -1,4 +1,4 @@
-update.dyads <- function(A, B, A_choice, B_choice, matrix, dyads, theory){
+update.dyads <- function(A, B, A_choice, B_choice, matrix, dyads){
 
 # find the box the agents end with
   if        (A_choice == "LA1" & B_choice == "LB1") {
@@ -20,34 +20,33 @@ B_outcome <- matrix$outcome[bo]
 
 # update dyads: sat, dep, tick_tog and outcome
 A_B <- which(dyads$agent_1 == A & dyads$agent_2 == B) # find the row no for the relevant dyad
-
-dyads$ticks_tog[A_B] <- dyads$ticks_tog[A_B] + 1 # add a tick to ticks spent together
-
-################## WOJNG LOOK INTO TRAITS!?!?!?
-
-dyads$sat[A_B] <- dyads$sat[A_B] + ((A_outcome - traits$cl_sat[A]) # update sat
-                                    /dyads$ticks_tog[A_B])
-
-dyads$dep[A_B] <- dyads$dep[A_B] + ((A_outcome - traits$cl_alt[A]) # update dep
-                                    /dyads$ticks_tog[A_B])
-
-dyads$commit[A_B] <- dyads$commit[A_B] + (traits$inv[A] * dyads$ticks_tog[A_B])
-
 dyads$outcome[A_B] <- A_outcome # add the outcome
+dyads$ticks_tog[A_B] <- dyads$ticks_tog[A_B] + 1 # add a tick to ticks spent together
+A_trait_index <- which(traits$agent == A) # Get agent A's index in traits
+
+
+dyads$sat[A_B] <- dyads$sat[A_B] + ((A_outcome - traits$cl_sat[A_trait_index]) # update sat
+                                    /dyads$ticks_tog[A_B])
+
+dyads$dep[A_B] <- dyads$dep[A_B] + ((A_outcome - traits$cl_alt[A_trait_index]) # update dep
+                                    /dyads$ticks_tog[A_B])
+
+dyads$commit[A_B] <- dyads$commit[A_B] + (traits$inv[A_trait_index] * dyads$ticks_tog[A_B]) # update commit
 
 # repeat for B
-
 B_A <- which(dyads$agent_1 == B & dyads$agent_2 == A) # find the row no for the relevant dyad
-
-dyads$ticks_tog[B_A] <- dyads$ticks_tog[B_A] + 1 # add a tick to ticks spent together
-
-dyads$sat[B_A] <- dyads$sat[B_A] + ((A_outcome - traits$cl_sat[B]) # update sat
-                                    /dyads$ticks_tog[B_A])
-
-dyads$dep[B_A] <- dyads$dep[B_A] + ((A_outcome - traits$cl_alt[B]) # update dep
-                                    /dyads$ticks_tog[B_A])
-
 dyads$outcome[B_A] <- B_outcome # add the outcome
+dyads$ticks_tog[B_A] <- dyads$ticks_tog[B_A] + 1 # add a tick to ticks spent together
+B_trait_index <- which(traits$agent == B) # Get agent B's index in traits
+
+
+dyads$sat[B_A] <- dyads$sat[B_A] + ((B_outcome - traits$cl_sat[B_trait_index]) # update sat
+                                    /dyads$ticks_tog[B_A])
+
+dyads$dep[B_A] <- dyads$dep[B_A] + ((B_outcome - traits$cl_alt[B_trait_index]) # update dep
+                                    /dyads$ticks_tog[B_A])
+
+dyads$commit[B_A] <- dyads$commit[B_A] + (traits$inv[B_trait_index] * dyads$ticks_tog[B_A]) # update commit
 
 return(dyads)
 }
